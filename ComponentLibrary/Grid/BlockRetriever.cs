aeeab3d43d6d9fe7075a2eef450b024ba5ec069a
Roger de Lange <rogerdlange@gmail.com>
2021-04-24 15:02:58 +0200

    Initial commit

---
+++ ComponentLibrary/Grid/BlockRetriever.cs
@@ -0,0 +1,43 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public static class BlockRetriever
+        {
+            private static List<IMyEntity> blocks;
+
+            public static IEnumerable<T> GetBlocksOfType<T>(Program p, bool mainBlocksOnly = true) where T : IMyEntity
+            {
+                if (blocks == null)
+                    PopulateInternalList(p);
+                return (IEnumerable<T>)blocks.Where(r => r.GetType() == typeof(T));
+            }
+
+            private static void PopulateInternalList(Program p)
+            {
+                blocks = new List<IMyEntity>();
+                p.GridTerminalSystem.GetBlocksOfType(blocks);
+            }
+        }
+    }
+}
